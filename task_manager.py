060e421687198a143cefe21f3dcc3e851c8c8bd7
vahid <vahid@mail.com>
2026-02-17 10:22:54 +0330

    shwo def is added

--- task_manager.py
+++ task_manager.py
@@ -2,5 +2,14 @@ task = []
 
 def add_task(user_input):
     task.append(user_input)
-add_task("kharid nan")
-print(task)
+
+def show_tasks():
+    for i in range(len(task)):
+        print(f"{i+1}. {task[i]}")
+
+add_task("خرید نان")
+add_task("ورزش کردن")
+add_task("مطالعه کتاب")
+
+show_tasks()
+
