883c31cc5f3e6176189c9b532a31e7eeea25b351
vahid <vahid@mail.com>
2026-02-17 10:44:03 +0330

    del def is added

--- task_manager.py
+++ task_manager.py
@@ -3,13 +3,23 @@ task = []
 def add_task(user_input):
     task.append(user_input)
 
+
+def del_task(item):
+
+    for i in task:
+        if i == item:
+            task.remove(i)
+
 def show_tasks():
     for i in range(len(task)):
-        print(f"{i+1}. {task[i]}")
+        print(f"{i+1}. *+* {task[i]}")
 
-add_task("خرید نان")
-add_task("ورزش کردن")
-add_task("مطالعه کتاب")
+
+add_task("nan")
+add_task("varzesh")
+add_task("ketab")
+
+del_task("nan")
 
 show_tasks()
 
